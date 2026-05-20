8ab7b2012c3b306f444f5648512a02c3c88dc0d6
Lakshmi-priya1 <21203030@rmd.ac.in>
2026-05-20 14:35:27 +0530

    Added dept

--- src/main/java/com/example/taskManagmentSystem/Service/UserService.java
+++ src/main/java/com/example/taskManagmentSystem/Service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Service
